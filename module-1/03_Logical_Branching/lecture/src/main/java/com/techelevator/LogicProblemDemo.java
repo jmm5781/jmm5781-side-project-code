54a5e042f24fee3630983cb34351f58759e9ede3
Yoav Morahg <yoav@techelevator.com>
2024-03-13 06:49:04 -0400

    m1d3 lecture

---
+++ module-1/03_Logical_Branching/lecture/src/main/java/com/techelevator/LogicProblemDemo.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+public class LogicProblemDemo {
+
+
+    /*
+        If the value is greater than 10 and either modifier1 or modifier2 is greater than 10
+        and addFIve is true return the String "value + 5"
+
+        If the value is greater than 10 and either modifier1 or modifier2 is greater than 10
+        and addFive is false return the String "value + 10"
+
+        If the value is less than 10 return the String "value",
+        except if the value is less than 0 return the String "negative"
+
+        In any other case return the String "no value"
+
+        Examples:
+        getCalculatedValue(11, 11, 9, true) ➔ "value + 5"
+        getCalculatedValue(11, 8, 25, true) ➔ "value + 5"
+        getCalculatedValue(11, 11, 9, false) ➔ "value + 10"
+        getCalculatedValue(8, 9, 26, true) ➔ "value"
+        getCalculatedValue(-1, 9, 26, true) ➔ "negative"
+        getCalculatedValue(-5, 11, 9, false) ➔ "negative"
+        getCalculatedValue(12, 7, 9, true) ➔ "no value"
+
+     */
+
+    public String getCalculatedValue(int value, int modifier1, int modifier2, boolean addFive) {
+        return "";
+    }
+}
