cf47d0f7c13a8c5d4b899509ad861f5eb18f399f
Jeremy McKeever <jeremymck@gmail.com>
2024-03-14 06:48:36 -0400

    first commit with one exercises 1-3 completed

--- module-1/02_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/02_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
@@ -47,7 +47,9 @@ public class Exercises {
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
-
+		int numberOfRaccoons = 3;
+		int numberOfRaccoonsThatGoHome = 2;
+		int numberOfRaccoonsLeftInWoods = numberOfRaccoons - numberOfRaccoonsThatGoHome;
 
         /* Exercise 4
         4. There are 5 flowers and 3 bees. How many less bees than flowers?
