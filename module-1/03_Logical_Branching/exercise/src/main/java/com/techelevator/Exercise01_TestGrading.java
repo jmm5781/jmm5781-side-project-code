cc4be3c1c8f301df95520bfbf3c34d36ab74b4ae
Jeremy McKeever <jeremymck@gmail.com>
2024-03-14 21:21:33 -0400

    m1d03 6/22 tests passing

--- module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise01_TestGrading.java
+++ module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise01_TestGrading.java
@@ -18,7 +18,7 @@ public class Exercise01_TestGrading {
     gradeTestPassFail(45) ➔ false
      */
     public boolean gradeTestPassFail(int score) {
-        return false;
+        return score >= 70;
     }
 
     /*
@@ -37,8 +37,16 @@ public class Exercise01_TestGrading {
     gradeTestNumeric(10) ➔ 0
      */
     public int gradeTestNumeric(int score) {
-        return 0;
-    }
+        if(score >= 90) {
+            return 3;
+        } else if(score >= 50) {
+            return 2;
+        } else if(score >= 25) {
+            return 1;
+        } else {
+            return 0;
+        }
+}
 
     /*
     Grade-o-matic has received even more requests to grade using the classic letter scale.
@@ -56,6 +64,16 @@ public class Exercise01_TestGrading {
     gradeTestLetter(45) ➔ 'F'
      */
     public char gradeTestLetter(int score) {
-        return ' ';
+        if(score >= 90) {
+            return 'A';
+        } else if(score >= 80) {
+            return 'B';
+        } else if(score >= 70) {
+            return 'C';
+        }  else if(score >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
     }
 }
