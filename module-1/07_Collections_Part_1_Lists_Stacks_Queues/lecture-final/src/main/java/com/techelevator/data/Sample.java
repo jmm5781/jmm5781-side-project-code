99caa99f33b3fafc2dd81c41f5b6d50be4b4b6bf
Yoav Morahg <yoav@techelevator.com>
2024-03-19 11:00:51 -0400

    m1d7 lecture-final + m1d8 tutorial

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture-final/src/main/java/com/techelevator/data/Sample.java
@@ -0,0 +1,17 @@
+package com.techelevator.data;
+
+import com.techelevator.ScratchPad;
+
+import java.util.List;
+
+public class Sample {
+
+    ScratchPad pad = new ScratchPad();
+
+    List<String> myLiat;
+
+    // AWT List
+
+    java.awt.List otherList;
+
+}
