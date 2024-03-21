f172c431456a74e6931e6ceabe4222200f8c9a6e
Yoav Morahg <yoav@techelevator.com>
2024-03-21 13:37:31 -0400

    cleaned up comments in lecture-final

--- module-1/09_Classes_and_Encapsulation/lecture-final/src/main/java/com/techelevator/Demo.java
+++ module-1/09_Classes_and_Encapsulation/lecture-final/src/main/java/com/techelevator/Demo.java
@@ -18,73 +18,26 @@ public class Demo {
 //        student1.name = "Yoav";
         System.out.println();
 //        student1.name = "JOhn"
-        student1.setName("William Shakespear");
-        student1.setName("John");
-        student1.setAge(33);
-        student1.setEnrollementStatus(0);
-
-        System.out.println();
-        System.out.println("Name: " + student1.getName());
-
-        student1.setEnrollementStatus(0);;
-        System.out.println(student1.getStatusDescription());
-
-        TechElevatorStudent student2 = new TechElevatorStudent();
-        student2.setName("Linda");
-
-        // Creating a second student
-
-        System.out.println("Student 1 name: " + student1.getName());
-        System.out.println("Student 2 name: " + student2.getName());
-
-
-
-        TechElevatorStudent student3 = new TechElevatorStudent("Cindy", 45);
-
-
-        TechElevatorStudent student4 = new TechElevatorStudent("Bob", "YM33", 54, 0, false);
-        System.out.println();
-
-
-        student1.sayMyName();
-        student3.sayMyName();
-
-
-        student1.setEnrollementStatus(TechElevatorStudent.INTERESTED);
-        student3.setEnrollementStatus(TechElevatorStudent.ENROLLED);
-
-        student1.moveToNextEntollmentStep();
-        student3.moveToNextEntollmentStep();
-
-        System.out.println("Student 1:");
-        System.out.println(student1);
-
-//        TechElevatorStudent.sayMyName();
-        student1.sayMyName();
-
-        System.out.println(student1.getWelcomeMessage());
-        System.out.println(student2.getWelcomeMessage());
-        System.out.println(student2.getWelcomeMessage());
-        TechElevatorStudent.setWelcomeMessageText("Welcome to TE");
-        System.out.println(student1.getWelcomeMessage());
-        System.out.println(student2.getWelcomeMessage());
-
-        System.out.println(TechElevatorStudent.getStatusDescriptionFromStatusCode(0));
+//        student1.setName("William Shakespear");
 
 
         /*
         System.out.println();
         System.out.println("################# Get data using setter methods #######################");
+         */
         // Set the instance data using setter methods - these are the instance's properties or state
-        */
 
+        student1.setName("John");
+        student1.setAge(33);
+        student1.setEnrollementStatus(0);
 
-
-        /*
+          /*
         System.out.println();
         System.out.println("################# Get data using getter methods #######################");
         // Read instance data using getter methods
         */
+        System.out.println();
+        System.out.println("Name: " + student1.getName());
 
 
         /*
@@ -92,6 +45,41 @@ public class Demo {
         System.out.println("################# Get data using derived getter methods #######################");
         // Getters can be derived - not tied to a property but based on other properties
         */
+        student1.setEnrollementStatus(0);;
+        System.out.println(student1.getStatusDescription());
+
+
+        // Creating a second student
+
+        TechElevatorStudent student2 = new TechElevatorStudent();
+        student2.setName("Linda");
+
+        /*
+        System.out.println("##############################################################################");
+        System.out.println("        The getters for each instance return the properties of THAT INSTANCE. ");
+        System.out.println("        Each instance can have different properties");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+
+        System.out.println("Student 1 name: " + student1.getName());
+        System.out.println("Student 2 name: " + student2.getName());
+
+         /*
+        System.out.println("##############################################################################");
+        System.out.println("        We can create constructors with arguments to initialize");
+        System.out.println("        data during creation (args constructor");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+
+        TechElevatorStudent student3 = new TechElevatorStudent("Cindy", 45);
+
+
+        TechElevatorStudent student4 = new TechElevatorStudent("Bob", "YM33", 54, 0, false);
+        System.out.println();
 
 
         /*
@@ -104,40 +92,7 @@ public class Demo {
         // Call instance methods (these are the class' behavior)
 
          */
-
-
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("        Classes can override toString method so that");
-        System.out.println("        when printing, the object will print meaningful");
-        System.out.println("        data rather than it's reference location");
-        System.out.println("##############################################################################");
-        System.out.println();
-
-         */
-
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("        We can create constructors with arguments to initialize");
-        System.out.println("        data during creation (args constructor");
-        System.out.println("##############################################################################");
-        System.out.println();
-
-         */
-
-
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("        The getters for each instance return the properties of THAT INSTANCE. ");
-        System.out.println("        Each instance can have different properties");
-        System.out.println("##############################################################################");
-        System.out.println();
-
-         */
-
+        student1.sayMyName();
 
         /*
 
@@ -149,6 +104,79 @@ public class Demo {
         System.out.println();
 
          */
+        student1.sayMyName();
+        student3.sayMyName();
+
+
+        student1.setEnrollementStatus(TechElevatorStudent.INTERESTED);
+        student3.setEnrollementStatus(TechElevatorStudent.ENROLLED);
+
+        student1.moveToNextEntollmentStep();
+        student3.moveToNextEntollmentStep();
+
+        /*
+        System.out.println("##############################################################################");
+        System.out.println("        Classes can override toString method so that");
+        System.out.println("        when printing, the object will print meaningful");
+        System.out.println("        data rather than it's reference location");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+        System.out.println("Student 1:");
+        System.out.println(student1);
+
+//        TechElevatorStudent.sayMyName();
+        student1.sayMyName();
+
+            /*
+        System.out.println("##############################################################################");
+        System.out.println("        Class [static] variables (like welcomeMessageText) have ONE value ");
+        System.out.println("        across ALL instances");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+
+        System.out.println(student1.getWelcomeMessage());
+        System.out.println(student2.getWelcomeMessage());
+        System.out.println(student2.getWelcomeMessage());
+
+          /*
+        System.out.println("##############################################################################");
+        System.out.println("        Class setters can be used to modify class data but ");
+        System.out.println("        this should generally be avoided");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+        TechElevatorStudent.setWelcomeMessageText("Welcome to TE");
+
+
+        // Changing above changes message for ALL instances
+        System.out.println(student1.getWelcomeMessage());
+        System.out.println(student2.getWelcomeMessage());
+
+        /*
+        System.out.println("##############################################################################");
+        System.out.println("     Class [static] methods (like getStatusDescriptionFromStatusCode) do not require  ");
+        System.out.println("     an instance and are called using the class as well");
+        System.out.println("##############################################################################");
+        System.out.println();
+
+         */
+
+        System.out.println(TechElevatorStudent.getStatusDescriptionFromStatusCode(0));
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -163,37 +191,6 @@ public class Demo {
         // to a default String
 
          */
-
-
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("        Class variables (like welcomeMessageText) have ONE value ");
-        System.out.println("        across ALL instances");
-        System.out.println("##############################################################################");
-        System.out.println();
-
-         */
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("     Class methods (like getStatusDescriptionFromStatusCode) do not require  ");
-        System.out.println("     an instance and are called using the clasa as well");
-        System.out.println("##############################################################################");
-        System.out.println();
-
-         */
-
-
-        /*
-        System.out.println("##############################################################################");
-        System.out.println("        Class setters can be used to modify class data but ");
-        System.out.println("        this should generally be avoided");
-        System.out.println("##############################################################################");
-        System.out.println();
-        
-         */
-
 
         /*
         System.out.println("##############################################################################");
